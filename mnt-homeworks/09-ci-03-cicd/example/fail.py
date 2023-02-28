a9cbd1c770c04837bf38fff32a6f7f74e1f37a6d
Alexey Mentukov <mentuk@gmail.com>
2023-02-28 06:57:40 +0000

    09-ci-03-cicd

--- mnt-homeworks/09-ci-03-cicd/example/fail.py
+++ mnt-homeworks/09-ci-03-cicd/example/fail.py
@@ -1,11 +1,11 @@
-def increment(index):
+def increment(index=0):
     index =+ 1
     return index
 def get_square(numb):
     return numb*numb
 def print_numb(numb):
     print("Number is {}".format(numb))
-    pass
+
 
 index = 0
 while (index < 10):
